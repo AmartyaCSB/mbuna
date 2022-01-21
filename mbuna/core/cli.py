534e789b07b225f687b1acb1f8d7d7b2084680d1
tlancaster <tlancaster6@gatech.edu>
2022-01-21 14:20:28 -0500

    initial commit

--- mbuna/core/cli.py
+++ mbuna/core/cli.py
@@ -2,8 +2,9 @@ import mbuna.constants as coc
 import fiftyone.core.cli as focli
 from yolov5.detect import run as detect
 from yolov5.export import run as export
-from yolov5.train import main as train
+from yolov5.train import run as train
 from yolov5.val import run as val
+import os
 
 
 class MbunaCommand(focli.Command):
@@ -87,15 +88,101 @@ class TrainCommand(focli.Command):
 
     @staticmethod
     def execute(parser, args):
-        print(args)
-        print('\n\n')
-        print(parser.parse_args())
-        pass
+        train(**vars(args))
 
+class ValCommand(focli.Command):
+    @staticmethod
+    def setup(parser):
+        parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='dataset.yaml path')
+        parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+        parser.add_argument('--batch-size', type=int, default=32, help='batch size')
+        parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='inference size (pixels)')
+        parser.add_argument('--conf-thres', type=float, default=0.001, help='confidence threshold')
+        parser.add_argument('--iou-thres', type=float, default=0.6, help='NMS IoU threshold')
+        parser.add_argument('--task', default='val', help='train, val, test, speed or study')
+        parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+        parser.add_argument('--single-cls', action='store_true', help='treat as single-class dataset')
+        parser.add_argument('--augment', action='store_true', help='augmented inference')
+        parser.add_argument('--verbose', action='store_true', help='report mAP by class')
+        parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+        parser.add_argument('--save-hybrid', action='store_true', help='save label+prediction hybrid results to *.txt')
+        parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+        parser.add_argument('--save-json', action='store_true', help='save a COCO-JSON results file')
+        parser.add_argument('--project', default='runs/val', help='save to project/name')
+        parser.add_argument('--name', default='exp', help='save to project/name')
+        parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+        parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    @staticmethod
+    def execute(parser, args):
+        val(**vars(args))
 
-def main():
+class DetectCommand(focli.Command):
+    @staticmethod
+    def setup(parser):
+        parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model path(s)')
+        parser.add_argument('--source', type=str, default=os.path.join(coc.BASE_DIR, 'data/images'), help='file/dir/URL/glob, 0 for webcam')
+        parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+        parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+        parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+        parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+        parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+        parser.add_argument('--view-img', action='store_true', help='show results')
+        parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+        parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+        parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+        parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+        parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
+        parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+        parser.add_argument('--augment', action='store_true', help='augmented inference')
+        parser.add_argument('--visualize', action='store_true', help='visualize features')
+        parser.add_argument('--update', action='store_true', help='update all models')
+        parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+        parser.add_argument('--name', default='exp', help='save results to project/name')
+        parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+        parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+        parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+        parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+        parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+        parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+
+    @staticmethod
+    def execute(parser, args):
+        detect(**vars(args))
+
+class ExportCommand(focli.Command):
+    @staticmethod
+    def setup(parser):
+        parser.add_argument('--data', type=str, default=os.path.join(coc.BASE_DIR, 'data/coco128.yaml'), help='dataset.yaml path')
+        parser.add_argument('--weights', type=str, default='yolov5s.pt', help='weights path')
+        parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640, 640], help='image (h, w)')
+        parser.add_argument('--batch-size', type=int, default=1, help='batch size')
+        parser.add_argument('--device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+        parser.add_argument('--half', action='store_true', help='FP16 half-precision export')
+        parser.add_argument('--inplace', action='store_true', help='set YOLOv5 Detect() inplace=True')
+        parser.add_argument('--train', action='store_true', help='model.train() mode')
+        parser.add_argument('--optimize', action='store_true', help='TorchScript: optimize for mobile')
+        parser.add_argument('--int8', action='store_true', help='CoreML/TF INT8 quantization')
+        parser.add_argument('--dynamic', action='store_true', help='ONNX/TF: dynamic axes')
+        parser.add_argument('--simplify', action='store_true', help='ONNX: simplify model')
+        parser.add_argument('--opset', type=int, default=13, help='ONNX: opset version')
+        parser.add_argument('--topk-per-class', type=int, default=100, help='TF.js NMS: topk per class to keep')
+        parser.add_argument('--topk-all', type=int, default=100, help='TF.js NMS: topk for all classes to keep')
+        parser.add_argument('--iou-thres', type=float, default=0.45, help='TF.js NMS: IoU threshold')
+        parser.add_argument('--conf-thres', type=float, default=0.25, help='TF.js NMS: confidence threshold')
+        parser.add_argument('--include', nargs='+',
+                            default=['torchscript', 'onnx'],
+                            help='available formats are (torchscript, onnx, coreml, saved_model, pb, tflite, tfjs)')
+    @staticmethod
+    def execute(parser, args):
+        export(**vars(args))
+
+def main(args=None):
     """Executes the `mbuna` tool with the given command-line args."""
     parser = focli._register_main_command(MbunaCommand, version=coc.VERSION_LONG)
-    args = parser.parse_args()
+    if args is None:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(args)
+    print(args)
     args.execute(args)
 
