719eb65aede5ec93a66506fd878bf8debedc14da
tlancaster6 <tlancaster6@gatech.edu>
2022-02-02 10:46:27 -0500

    51 practice

--- notebooks/annotation.py
+++ notebooks/annotation.py
@@ -7,12 +7,10 @@ import fiftyone as fo
 import fiftyone.brain as fob
 import os
 
-if 'demo' in fo.list_datasets():
-    fo.delete_dataset('demo')
 
-dataset_dir = os.path.join(DATA_DIR, 'demo')
+dataset_dir = os.path.join(DATA_DIR, 'kl')
 img_dir = os.path.join(dataset_dir, 'images')
-name = 'demo'
+name = 'kl'
 
 dataset = fo.Dataset.from_dir(
     dataset_dir=img_dir,
@@ -26,7 +24,7 @@ anno_key = 'demo_anno'
 anno_results = dataset.annotate(
     anno_key=anno_key,
     label_type='detections',
-    label_field='sex',
-    classes=['male', 'female'],
+    label_field='detect_ground_truth',
+    classes=['fish'],
     launch_editor=True
 )
